6a7f8decce36616a4cf5f595029bb772156b03ad
zawi99 <o2zawio2@gmail.com>
2018-12-05 13:02:37 +0100

    Solved part 2 of day 2

--- 2018/day2part2.py
+++ 2018/day2part2.py
@@ -4,4 +4,13 @@ from utils import get_input
 
 data = get_input(2, 2018).splitlines()
 
+for box1 in data:
+    for box2 in data:
+        diff = 0
+        for idx, char in enumerate(box1):
+            if char != box2[idx]:
+                diff += 1
 
+        if diff == 1:
+            common_letters = [char for idx, char in enumerate(box1) if char == box2[idx]]
+            print(''.join(common_letters))
