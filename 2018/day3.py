bfde3fcf568e318d1b1ce8ad923b17163db4e40d
zawi99 <o2zawio2@gmail.com>
2018-12-06 19:07:50 +0100

    Solved part 1 of day 3

---
+++ 2018/day3.py
@@ -0,0 +1,28 @@
+# https://adventofcode.com/2018/day/3
+
+from utils import get_input
+from collections import defaultdict
+
+# #40 @ 416,470: 24x11
+
+data = get_input(3, 2018).splitlines()
+
+C = defaultdict(int)
+
+for line in data:
+    words = line.split()
+    x, y = words[2].split(',')
+    x, y = int(x), int(y[:-1])
+    w, h = words[3].split('x')
+    w, h = int(w), int(h)
+    for dx in range(w):  # go through all points of box
+        for dy in range(h):
+            C[(x + dx, y + dy)] += 1  # if any point is overlaps with another add 1
+
+overlapped_squares = 0
+for value in C.values():
+    if value > 1:  # if true -> square belongs to at least two or more boxes == overlap
+        overlapped_squares += 1
+
+print(f'Part 1 result: {overlapped_squares}')
+
